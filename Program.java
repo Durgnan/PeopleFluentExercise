e25846124f157af7bb8ad169a57a4dfe27ff6871
Durgesh Nandan <durgeshnandan@Durgeshs-MacBook-Pro.local>
2023-03-12 02:51:31 +0530

    Step 2 Initial Approach

--- Program.java
+++ Program.java
@@ -15,29 +15,48 @@ class Program {
         {
             String item = items[i];
             totalCost += item.equals("Apple") ? appleCost : item.equals("Orange") ? orangeCost : 0;
-            System.out.println(""+totalCost);
         }
-
-        
 
         return "£"+totalCost;
     }
+
+    public static String calculateTotalCostWithOffers(String[] items)
+    {
+        int totalApples = 0;
+        int totalOranges = 0;
+        double appleCost = 0.3;
+        double orangeCost = 0.5/3;
+        double totalCost = 0;
+
+        for(int i = 0; i < items.length; i++)
+        {
+            String item = items[i];
+            totalCost += item.equals("Apple") ? appleCost : item.equals("Orange") ? orangeCost : 0;
+        }
+
+        return "£"+ Math.round(totalCost*100.0)/100.0;
+    }
+
+
+
     public static void main(String[] args) {
         ArrayList<String> items = new ArrayList<String>();
         String item;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Items. \n Press 0 to Stop");
+        System.out.println("Enter Your Items.\nPress 0 to Stop");
         while(true) {
             item = sc.nextLine();     
             if(item.equals("0"))
                 break;       
             items.add(item);
         }
-        System.out.println(""+items.toString());
+
         String[] itemArray = Arrays.copyOf(items.toArray(),items.size(), String[].class);
+        // Step 1 Result 
         System.out.println("Total Cost: "+ calculateTotalCost(itemArray));
-
-
+        // Step 2 Result 
+        System.out.println("Total Cost with offers: "+ calculateTotalCostWithOffers(itemArray));
+        sc.close();
 
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 #### PeopleFluent Coding Exercise
 
+##### Steps to Run
+
+1. Clone the Repo
+2. CD in into it 
+3. ``` shell$ javac Program.java ```
+4. ``` shell$ java Program ```
+5. Enter all the items with each items in new line
+6. Enter 0 when the list has ended
+7. The Program will give two results one with offers and one without.
+
+
+
+###### NB: Different commits are tagged with different approaches of each steps in Document
